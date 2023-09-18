9874f35dfd833d90252d1fb5f21fa0f1bd524f09
Afif <mk.afifmuhammad@gmail.com>
2023-09-18 22:28:11 +0530

    axios changed 2nd time

--- src/Redux/Reducers/ApiUrlReducers.js
+++ src/Redux/Reducers/ApiUrlReducers.js
@@ -1,18 +1,21 @@
-// const initialstate={
-//     url:"https://m4matrimony.online"
-// }
-
-
-// const ApiUrlReducer=(state=initialstate,action)=>{
-//     return state
-    
-// }
-
-
-// export default ApiUrlReducer
-
-
 import axios from 'axios';
+
+
+const initialstate={
+    url:"https://m4matrimony.online"
+}
+
+
+const ApiUrlReducer=(state=initialstate,action)=>{
+    return state
+    
+}
+
+
+export default ApiUrlReducer
+
+
+
 
 // const initialstate = {
 //     url: "https://m4matrimony.online"
