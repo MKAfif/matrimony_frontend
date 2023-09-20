be796b9cb57f013e7ce56a618fba6f014802f786
Afif <mk.afifmuhammad@gmail.com>
2023-09-20 19:51:16 +0530

    url again changed

--- src/Redux/Reducers/ApiUrlReducers.js
+++ src/Redux/Reducers/ApiUrlReducers.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 
 const initialstate={
-    url:"https://m4matrimony.online"
-    
+    // url:"https://m4matrimony.online"
+    // url : " http://127.0.0.1:8000"
+    url : "http://15.206.179.168:8000"
 }
 
 
