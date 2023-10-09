4f88556b0150cd55064927715563155361497b80
Afif <mk.afifmuhammad@gmail.com>
2023-10-09 15:31:30 +0530

    api slash  changed

--- src/Redux/Reducers/ApiUrlReducers.js
+++ src/Redux/Reducers/ApiUrlReducers.js
@@ -4,7 +4,7 @@
 const initialstate={
     // url: "https://m4matrimony.online"
     // url : "http://127.0.0.1:8000"
-    url : "https://backend-matrimony-1z1t.onrender.com/"
+    url : "https://backend-matrimony-1z1t.onrender.com"
     
 }
 
